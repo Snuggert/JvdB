f9231473f6093ddb5a5d56286cbd64031b0841df
Snuggert <abe.wiersma@hotmail.nl>
2014-06-05 00:33:22 +0200

    home-page??

--- jvdb/views/views.py
+++ jvdb/views/views.py
@@ -8,6 +8,7 @@ views_blueprint = Blueprint('views', __name__, url_prefix='')
 
 @views_blueprint.route('/works', methods=['GET'])
 @views_blueprint.route('/', methods=['GET'])
+@views_blueprint.route('', methods=['GET'])
 def works():
     sql_pieces = PieceController.get_all()
     pieces = []
